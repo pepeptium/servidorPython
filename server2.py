bafb450c0defd1a9a291026d0f0f5c82b2dd7999
pepeptium <119102609+pepeptium@users.noreply.github.com>
2025-10-12 00:42:26 +0200

    Add files via upload

--- server2.py
+++ server2.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import io
 import pandas as pd
+import datetime
+
 from io import BytesIO
 app = FastAPI()
 app.add_middleware(
