12f694e37378ae8ad5f64ed95859fed767062d93
pepeptium <119102609+pepeptium@users.noreply.github.com>
2025-10-11 18:40:21 +0200

    Delete wgsi.py

--- wgsi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Ajusta el path si tu archivo está en una subcarpeta
-path = '/home/pepeptium/servidorPython'
-if path not in sys.path:
-    sys.path.append(path)
-
-from asgi import application  # importa desde asgi.py
